692cdda21a5dfd704a4879794342f01230b61288
anagarwa <anagarwa@adobe.com>
2022-12-07 00:05:09 +0530

    updating stage environment

--- web-src/src/components/Home.js
+++ web-src/src/components/Home.js
@@ -5,10 +5,12 @@
 import React from 'react'
 import { Heading, View } from '@adobe/react-spectrum'
 import Text from './Text';
+const REACT_VERSION = React.version;
 export const Home = () => (
   <View width='size-6000'>
+      <div>React version: {REACT_VERSION}</div>
     <Heading level={1}>Welcome to franklinreact spa!</Heading>
-      <p itemID="urn:aemconnection:/content/xyz/en/jcr:content/root/container/container/teaser" itemProp="jcr:title" itemType="text">Photographer</p>
-      <Text itemID="urn:aemconnection:/content/xyz/en/jcr:content/root/container/container/teaser" itemProp="jcr:title" itemType="text"/>
+      <p itemID="urn:aemconnection:/content/wknd/language-masters/en/jcr:content" itemProp="jcr:title" itemType="text">Photographer</p>
+      <Text itemID="urn:aemconnection:/content/wknd/language-masters/de/jcr:content" itemProp="jcr:title" itemType="text"/>
   </View>
 )
--- web-src/src/components/fetchData.js
+++ web-src/src/components/fetchData.js
@@ -7,9 +7,14 @@ export const fetchData = async (path) => {
         const json = await data.json();
         return json.paths;
     } else {
-        const url = `https://author-p60703-e140888-cmstg.adobeaemcloud.com/${path.split(":/")[1]}.model.json`;
+        const url = `https://author-p60703-e140792-cmstg.adobeaemcloud.com/${path.split(":/")[1]}.1.json`;
         console.log ("url is " + path.split(":/")[1]);
-        const data = await fetch(url);
+        const data = await fetch(url, {
+                headers: new Headers({
+                    'Authorization': 'Basic '+btoa('anagarwa:1')
+                })
+            }
+        );
         const json = await data.json();
         return json;
     }
