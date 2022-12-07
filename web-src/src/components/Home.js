479e8a51807a9179414f8393a39fdf5316acb67f
anagarwa <anagarwa@adobe.com>
2022-12-07 12:56:07 +0530

    updating urls

--- web-src/src/components/Home.js
+++ web-src/src/components/Home.js
@@ -11,6 +11,7 @@ export const Home = () => (
       <div>React version: {REACT_VERSION}</div>
     <Heading level={1}>Welcome to franklinreact spa!</Heading>
       <p itemID="urn:aemconnection:/content/wknd/language-masters/en/jcr:content" itemProp="jcr:title" itemType="text">Photographer</p>
-      <Text itemID="urn:aemconnection:/content/wknd/language-masters/de/jcr:content" itemProp="jcr:title" itemType="text"/>
+      <Text itemID="urn:aemconnection:/content/videourls/url1" itemProp="url" itemType="text"/>
+      <Text itemID="urn:aemconnection:/content/videourls/url2" itemProp="url" itemType="text"/>
   </View>
 )
