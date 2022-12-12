060fa903a6e551ad2c98f5a439205255707cf426
anagarwa <anagarwa@adobe.com>
2022-12-12 16:13:54 +0530

    updating environment

--- web-src/src/components/fetchData.js
+++ web-src/src/components/fetchData.js
@@ -8,12 +8,11 @@ export const fetchData = async (path) => {
         const json = await data.json();
         return json.paths;
     } else {
-       // const url = `https://author-p60703-e140792-cmstg.adobeaemcloud.com/${path.split(":/")[1]}.1.json`;
         let url = "";
         if (path.includes(":/")) {
-            url = `https://author-p60703-e140792-cmstg.adobeaemcloud.com/${path.split(":/")[1]}.1.json`;
+            url = `https://author-p60703-e142381-cmstg.adobeaemcloud.com/${path.split(":/")[1]}.1.json`;
         } else {
-            url = `https://author-p60703-e140792-cmstg.adobeaemcloud.com/${path}.2.json`;
+            url = `https://author-p60703-e142381-cmstg.adobeaemcloud.com/${path}.2.json`;
         }
         console.log ("url1 is " + url);
         const data = await fetch(url, {
